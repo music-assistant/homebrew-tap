622e1ddb667a04b6482d0c9d69c6652fcd5c88a2
Jonathan Bangert <jonathan@bangert.dk>
2024-01-21 13:32:51 +0100

    Damn styling

--- Casks/companion.rb
+++ Casks/companion.rb
@@ -6,7 +6,7 @@ cask "companion" do
          intel: "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7"
 
   url "https://github.com/music-assistant/companion/releases/download/v#{version}/Music.Assistant.Companion_#{arch}.app.tar.gz",
-    verified: "https://github.com/music-assistant/companion"
+      verified: "github.com/music-assistant/companion/"
   name "Music-Assistant-Companion"
   desc "App for music assistant"
   homepage "https://music-assistant.github.io/"
