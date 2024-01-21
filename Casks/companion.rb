25724fb5ef7796b626d660b31a1c543a2a687f34
Jonathan Bangert <jonathan@bangert.dk>
2024-01-21 13:23:38 +0100

    Arm not aarch

--- Casks/companion.rb
+++ Casks/companion.rb
@@ -1,8 +1,8 @@
 cask "companion" do
-  arch aarch: "aarch64", intel: "x64"
+  arch arm: "aarch64", intel: "x64"
 
   version "0.0.31"
-  sha256 aarch: "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7",
+  sha256 arm: "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7",
          intel: "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7"
 
   url "https://github.com/music-assistant/companion/releases/download/v#{version}/Music.Assistant.Companion_#{arch}.app.tar.gz"
