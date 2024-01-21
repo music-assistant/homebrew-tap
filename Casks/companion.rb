c82fe6f63056dd41c47c5ddbcb9fda38afa9b536
Jonathan Bangert <jonathan@bangert.dk>
2024-01-21 13:20:09 +0100

    Fix styling

--- Casks/companion.rb
+++ Casks/companion.rb
@@ -1,15 +1,14 @@
 cask "companion" do
   arch aarch: "aarch64", intel: "x64"
 
-  name "Music-Assistant-Companion"
-  desc "Companion app for music assistant"
-  homepage "https://music-assistant.github.io/"
-
   version "0.0.31"
   sha256 aarch: "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7",
          intel: "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7"
 
   url "https://github.com/music-assistant/companion/releases/download/v#{version}/Music.Assistant.Companion_#{arch}.app.tar.gz"
+  name "Music-Assistant-Companion"
+  desc "App for music assistant"
+  homepage "https://music-assistant.github.io/"
 
   app "music-assistant-companion.app"
 end
