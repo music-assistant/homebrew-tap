bb76b9e237d840fd6b1576a2bb5d9135e97382a7
Jonathan Bangert <jonathan@bangert.dk>
2024-01-21 13:09:58 +0100

    aiosjd

--- Casks/companion.rb
+++ Casks/companion.rb
@@ -1,13 +1,13 @@
 cask "companion" do
-  arch arm: "arm64", intel: "x64"
+  arch aarch: "aarch64", intel: "x64"
 
   name "Music Assistant Companion"
   desc "Music Assistant Companion app"
   homepage "https://music-assistant.github.io/"
 
   version "0.0.31"
-  sha256 arm: "bla bla"
-         intel: "bla bla"
+  sha256 aarch: "bla bla",
+         intel: "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7"
 
   url "https://github.com/music-assistant/companion/releases/download/v#{version}/Music.Assistant.Companion_#{arch}.app.tar.gz"
 
