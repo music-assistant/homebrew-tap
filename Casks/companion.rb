7297c160e9b248875b1e131679c7ddc13dd16575
Jonathan Bangert <jonathan@bangert.dk>
2024-05-17 19:39:52 +0200

    Update companion.rb

--- Casks/companion.rb
+++ Casks/companion.rb
@@ -2,8 +2,8 @@ cask "companion" do
   arch arm: "aarch64", intel: "x64"
 
   version "0.0.45"
-  sha256 arm:   "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7",
-         intel: "0c4d4afc31ded13fafdc4b58b6ae9447856c706cad34e2b5c5cc7161213f50c7"
+  sha256 arm:   "b4c1fc54a4c741e72276125c51584b3d25c9e636e59b1fce71d7fddad51acc69",
+         intel: "b4c1fc54a4c741e72276125c51584b3d25c9e636e59b1fce71d7fddad51acc69"
 
   url "https://github.com/music-assistant/companion/releases/download/v#{version}/Music.Assistant.Companion_#{arch}.app.tar.gz",
       verified: "github.com/music-assistant/companion/"
