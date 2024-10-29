cb0e1b0301fee740e247f473817b84a8058741a5
Sander van Kasteel <info@sandervankasteel.nl>
2024-10-29 21:44:07 +0100

    Fixed sha256 checksums

--- Casks/companion.rb
+++ Casks/companion.rb
@@ -2,8 +2,8 @@ cask "companion" do
   arch arm: "aarch64", intel: "x64"
 
   version "0.0.74"
-  sha256 arm:   "463ee8245e8a93c569158617e14e76444b0d761c5a6025285c6f368292dfa2a2",
-         intel: "c0b0759f38abc9267720a662c53406b3365ef9893ea9bbb02729037179134ddd"
+  sha256 arm:   "a47b47d4ac238c95fe76ae94e0da64505571cc8d0e46726ef96e0ef6c83dcb50",
+         intel: "e7d011366b1a04ee533c9f79d913f8a47f84e69703bb17579574655d249a5275"
 
   url "https://github.com/music-assistant/companion/releases/download/v#{version}/Music.Assistant.Companion_#{arch}.app.tar.gz",
       verified: "github.com/music-assistant/companion/"
