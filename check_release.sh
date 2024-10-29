e2b0d0fad86384bb9e05125c21d1f1167f5633b2
Sander van Kasteel <info@sandervankasteel.nl>
2024-10-30 00:39:36 +0100

    Added basic script to grab latest version from Github with and do a sha256 on the files

---
+++ check_release.sh
@@ -0,0 +1,22 @@
+# !/usr/bin/env bash
+RELEASES=('AARCH64_SHA256' 'X64_SHA256')
+
+TAG_NAME=$(curl -fsL \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/music-assistant/companion/releases | jq '.[0].tag_name')
+
+export MUSIC_COMPANION_VERSION=$(echo "$TAG_NAME" | cut -d"v" -f 2 | cut -d"\"" -f 1)
+
+for i in "${RELEASES[@]}"
+do
+    ARCH=$(echo "$i" | cut -f1 -d_ | tr '[:upper:]' '[:lower:]')
+    URL="https://github.com/music-assistant/companion/releases/download/v0.0.74/Music.Assistant.Companion_${ARCH}.app.tar.gz"
+    curl -sLO $URL
+
+    SHA_FILE=$(shasum -a 256 "Music.Assistant.Companion_${ARCH}.app.tar.gz" | cut -d" " -f 1)
+
+    export $i=$SHA_FILE
+done
+
+cat Casks/companion.template.rb | envsubst > Casks/companion.rb
