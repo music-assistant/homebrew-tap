95009a597c49d1939fb1eadfaac24ccb228f9c74
Sander van Kasteel <info@sandervankasteel.nl>
2024-10-30 12:59:23 +0100

    More shellcheck fixes

--- check_release.sh
+++ check_release.sh
@@ -6,17 +6,18 @@ TAG_NAME=$(curl -fsL \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   https://api.github.com/repos/music-assistant/companion/releases | jq '.[0].tag_name')
 
-export MUSIC_COMPANION_VERSION=$(echo "$TAG_NAME" | cut -d"v" -f 2 | cut -d"\"" -f 1)
+MUSIC_COMPANION_VERSION=$(echo "${TAG_NAME}" | cut -d"v" -f 2 | cut -d"\"" -f 1)
+export MUSIC_COMPANION_VERSION
 
 for i in "${RELEASES[@]}"
 do
     ARCH=$(echo "${i}" | cut -f1 -d_ | tr '[:upper:]' '[:lower:]')
     URL="https://github.com/music-assistant/companion/releases/download/v${MUSIC_COMPANION_VERSION}/Music.Assistant.Companion_${ARCH}.app.tar.gz"
-    curl -sLO $URL
+    curl -sLO "${URL}"
 
     SHA_FILE=$(shasum -a 256 "Music.Assistant.Companion_${ARCH}.app.tar.gz" | cut -d" " -f 1)
 
-    export $i=$SHA_FILE
+    export "${i}"="${SHA_FILE}"
 done
 
-cat companion.template.rb | envsubst > Casks/companion.rb
+envsubst < companion.template.rb > Casks/companion.rb
