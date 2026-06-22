8b53029f1bf7454bac652d8a85ae29de449956d8
Yoeri Van Damme <yoeri.vandamme@gmail.com>
2026-06-22 10:00:01 +0200

    Update companion.rb to 3.9

--- Casks/companion.rb
+++ Casks/companion.rb
@@ -1,9 +1,9 @@
 cask "companion" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.3.7"
-  sha256 arm:   "e0d79dba10af72d1a6ef483ff6260807093c90cfd585cbd60d3c3907bc651293",
-         intel: "1f9be7b1293443b428b624b932dd913230cd87a1eceb31aec4bd56173374982f"
+  version "0.3.9"
+  sha256 arm:   "d35fa3d0941302e2bb979b2ea07b1c7c3e3ba73edf71c3a7812b3fbf3dbb2b26",
+         intel: "c588ece3b523777a248087e7af39d54c80d28620e65e15c4fd1716da29a47f70"
 
   url "https://github.com/music-assistant/desktop-app/releases/download/#{version}/Music.Assistant_#{arch}.app.tar.gz",
       verified: "github.com/music-assistant/desktop-app/"
