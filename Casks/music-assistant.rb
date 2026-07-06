387ebf9434497f8113dcbf09593bd7563a8473cb
David Bishop <git@gnuconsulting.com>
2026-07-06 12:26:23 -0700

    fix: correct cask stanza order

--- Casks/music-assistant.rb
+++ Casks/music-assistant.rb
@@ -1,8 +1,6 @@
 cask "music-assistant" do
   arch arm: "aarch64", intel: "x64"
 
-  depends_on :macos
-
   version "0.5.0"
   sha256 arm:   "bef193618413df444c51c812739d2a111b8ce37cf547fdd6e03d6c26d7c55e16",
          intel: "656db3a32e815ad06ee28363ae18dd854ea35451638fcb128689c7a084e4b29d"
@@ -12,6 +10,8 @@ cask "music-assistant" do
   name "Music Assistant"
   desc "Desktop app for Music Assistant"
   homepage "https://music-assistant.io/"
+
+  depends_on :macos
 
   app "Music Assistant.app"
 end
