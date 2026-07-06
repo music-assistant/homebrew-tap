ae50a3babd4825e162e8c22c87d57c99c8bad6aa
David Bishop <git@gnuconsulting.com>
2026-07-06 12:06:23 -0700

    fix: use the new release format, rename cask
    We are now releasing the files with the version string embedded in the file name, which the tap was not looking for. Also, we generally refer to the desktop client as Music Assistant, not Companion, so renaming the cask to match what people would be searching for (and what our documentation already called it). There will be an accompanying PR in the desktop-app repo to align the docs and naming convention fully on both sides.

---
+++ Casks/music-assistant.rb
@@ -0,0 +1,15 @@
+cask "music-assistant" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "0.5.0"
+  sha256 arm:   "bef193618413df444c51c812739d2a111b8ce37cf547fdd6e03d6c26d7c55e16",
+         intel: "656db3a32e815ad06ee28363ae18dd854ea35451638fcb128689c7a084e4b29d"
+
+  url "https://github.com/music-assistant/desktop-app/releases/download/#{version}/Music.Assistant_#{version}_#{arch}.app.tar.gz",
+      verified: "github.com/music-assistant/desktop-app/"
+  name "Music Assistant"
+  desc "Desktop app for Music Assistant"
+  homepage "https://music-assistant.io/"
+
+  app "Music Assistant.app"
+end
