3053dae159b20bf6789f5c1e5981f99c95f77ec7
David Bishop <git@gnuconsulting.com>
2026-07-06 12:19:35 -0700

    fix: Use modern upload action, bump cache action, remove dead linux references

--- Casks/music-assistant.rb
+++ Casks/music-assistant.rb
@@ -1,6 +1,8 @@
 cask "music-assistant" do
   arch arm: "aarch64", intel: "x64"
 
+  depends_on :macos
+
   version "0.5.0"
   sha256 arm:   "bef193618413df444c51c812739d2a111b8ce37cf547fdd6e03d6c26d7c55e16",
          intel: "656db3a32e815ad06ee28363ae18dd854ea35451638fcb128689c7a084e4b29d"
